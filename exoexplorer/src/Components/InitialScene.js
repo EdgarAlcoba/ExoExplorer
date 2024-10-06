cd128f8c41be98cb5a986f681aa2bbd65def8268
ealcoc00 <ealcoc00@estudiantes.unileon.es>
2024-10-06 15:31:41 +0200

    Solved style problem

--- exoexplorer/src/Components/InitialScene.js
+++ exoexplorer/src/Components/InitialScene.js
@@ -4,10 +4,10 @@ import text from "../Assets/Texts.json"
 
 function InitialScene({goToPlanetScene}) {
     return (
-        <>
+        <div style={{padding:"1rem"}}>
             <TypingText text={text.Intro+"\n "+text.Rules} speed={40} />
             <button className='continue-button' onClick={goToPlanetScene}>Start the adventure</button>
-        </>
+        </div>
     );
 }
 
--- exoexplorer/src/Components/PlanetScene.js
+++ exoexplorer/src/Components/PlanetScene.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 function PlanetScene({ planets, handlePlanetChoice }) {
   return (
-    <div style={{padding:"1rem"}}>
+    <div style={{ padding: "1rem" }}>
       <h2 style={{ color: "white" }}>Select the planet that you want to visit</h2>
       <p style={{ color: "white" }}>Your choice will matter</p>
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
         {planets.map((planet) => (
-          <button style={{margin:"5px"}} className='continue-button' key={planet.name} onClick={() => handlePlanetChoice(planet)}>
+          <button style={{
+            margin: "5px",
+            flex: '1 1 calc(40% - 10px)', // Permite más compresión
+            minWidth: '150px' // Ancho mínimo para que no se comprima demasiado
+          }} className='continue-button' key={planet.name} onClick={() => handlePlanetChoice(planet)}>
             <div>
               <h4>
                 {planet.name}
